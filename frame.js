1eb2dd4ad9a64ae6a024781655bb314f292be41a
Mattias Eriksson <lampkraft@gmail.com>
2016-06-09 22:25:05 +0200

    First version

---
+++ frame.js
@@ -0,0 +1,5 @@
+$(window).load(function()
+{
+	$(".frame-wrapper").prepend("<ul class=\"frame-top\"><li></li><li></li><li></li><li></li><li></li></ul><ul class=\"frame-left\"><li></li><li></li><li></li></ul>");
+	$(".frame-wrapper").append("<ul class=\"frame-right\"><li></li><li></li><li></li></ul><ul class=\"frame-bottom\"><li></li><li></li><li></li><li></li><li></li></ul>");
+});
